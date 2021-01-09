1f33e68964a5d711bab1c1ad0d390f6909ef610b
TheRealHoko <julien.zeybel@live.fr>
2021-01-09 21:05:00 +0100

    feat : added putbase (cimer romanito)

--- libft/put/ft_putbase_fd.c
+++ libft/put/ft_putbase_fd.c
@@ -1,37 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putbase_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jzeybel <jzeybel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/19 12:27:22 by jzeybel           #+#    #+#             */
-/*   Updated: 2021/01/09 19:22:51 by jzeybel          ###   ########.fr       */
+/*   Created: 2021/01/09 18:43:21 by jzeybel           #+#    #+#             */
+/*   Updated: 2021/01/09 20:54:14 by jzeybel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-void	ft_putnbr_fd(long long int n, int fd)
+void	ft_putbase_fd(int n, char *base, size_t size)
 {
-	unsigned long long int nn;
-
-	if (n >= 0 && n < 10)
-		ft_putchar_fd(n + 48, fd);
-	else if (n < 0)
-	{
-		nn = n * (-1);
-		if (nn == 9223372036854775808u)
-		{
-			write(fd, "-9223372036854775808", 21);
-			return ;
-		}
-		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(nn, fd);
-	}
-	else
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
+	if (n / (int)size > 0)
+		ft_putbase_fd(n / (int)size, base);
+	ft_putchar_fd(base[n % (int)size], 1);
 }

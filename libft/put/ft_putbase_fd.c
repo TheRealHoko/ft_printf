6a28be3f5b673f9323ba85da7a0441ecd4abd47d
TheRealHoko <julien.zeybel@live.fr>
2021-01-09 21:20:20 +0100

    fix : putbase

--- libft/put/ft_putbase_fd.c
+++ libft/put/ft_putbase_fd.c
@@ -6,7 +6,7 @@
 /*   By: jzeybel <jzeybel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 18:43:21 by jzeybel           #+#    #+#             */
-/*   Updated: 2021/01/09 20:54:14 by jzeybel          ###   ########.fr       */
+/*   Updated: 2021/01/09 21:20:04 by jzeybel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	ft_putbase_fd(int n, char *base, size_t size)
 {
 	if (n / (int)size > 0)
-		ft_putbase_fd(n / (int)size, base);
+		ft_putbase_fd(n / (int)size, base, (int)size);
 	ft_putchar_fd(base[n % (int)size], 1);
 }

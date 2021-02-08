47f43ccb4a0cf650957b7d2c04cd007d92849320
TheRealHoko <julien.zeybel@live.fr>
2021-02-08 17:49:06 +0100

    fix : prec and width used sim

--- src/ft_write_diupxX.c
+++ src/ft_write_diupxX.c
@@ -6,7 +6,7 @@
 /*   By: jzeybel <jzeybel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 12:42:27 by jzeybel           #+#    #+#             */
-/*   Updated: 2021/02/08 17:37:03 by jzeybel          ###   ########.fr       */
+/*   Updated: 2021/02/08 17:48:57 by jzeybel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,15 +116,18 @@ void	write_p(va_list ap, t_flags *flags)
 	i = va_arg(ap, unsigned long long);
 	if (!i)
 		flags->n = 0;
+	if (flags->zero && !flags->minus)
+		writestr_buf("0x", 2);
 	array = ft_ulltoa_base(i, "0123456789abcdef");
 	len = ft_strlen(array);
-	writestr_buf("0x", 2);
 	if (!flags->prec && !i)
 		len = 0;
 	if ((flags->width || flags->zero) && !flags->minus)
 		width(len + 2, flags);
 	else if (flags->prec > (len + 2))
 		prec(len + 2, flags);
+	if (!flags->zero)
+		writestr_buf("0x", 2);
 	if ((flags->prec != 0) || (i != 0))
 		writestr_buf(array, len);
 	if (flags->width && flags->minus)

c08470cfc72427624d10bbdd82cd42a4f9f86354
TheRealHoko <julien.zeybel@live.fr>
2021-02-08 15:47:13 +0100

    fix : prec and width used sim

--- src/ft_write_cspct.c
+++ src/ft_write_cspct.c
@@ -6,7 +6,7 @@
 /*   By: jzeybel <jzeybel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 16:32:18 by jzeybel           #+#    #+#             */
-/*   Updated: 2021/02/08 15:46:04 by jzeybel          ###   ########.fr       */
+/*   Updated: 2021/02/08 15:47:09 by jzeybel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	width(int len, t_flags *flags)
 	}
 	else
 		i = flags->width - len;
-	if (flags->zero && flags->c/*&& (flags->prec == -1)*/)
+	if (flags->zero && (flags->c || (flags->prec == -1)))
 	{
 		if (flags->sign == -1)
 			writec_buf('-');

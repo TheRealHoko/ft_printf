d6e971b5d1e942aa64c2805a57453abab15ca5ce
TheRealHoko <julien.zeybel@live.fr>
2021-02-09 16:13:35 +0100

    fix : prec and width used sim

--- src/ft_write_diupxX.c
+++ src/ft_write_diupxX.c
@@ -6,7 +6,7 @@
 /*   By: jzeybel <jzeybel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 12:42:27 by jzeybel           #+#    #+#             */
-/*   Updated: 2021/02/09 16:12:27 by jzeybel          ###   ########.fr       */
+/*   Updated: 2021/02/09 16:13:33 by jzeybel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,7 +123,7 @@ void	write_p(va_list ap, t_flags *flags)
 		len = 0;
 	if ((flags->width || flags->zero) && !flags->minus)
 	{
-		if (flags->zero || flags->width)
+		if (flags->zero)
 			writestr_buf("0x", 2);
 		width(len, flags);
 	}
